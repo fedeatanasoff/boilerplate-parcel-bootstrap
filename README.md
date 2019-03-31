2ac1453a9ea7e9200e13c2302e80e168a2be8552
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-31 00:23:59 -0300

    boilerplate v1

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1> Boilerplate con Parcel, Sass y Bootstrap v4 </h1>
+
+- npm install -g parcel-bundler
+- npm install
+- npm run dev
+
+server corriendo en http:localhost:1234
---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import { saludar } from "./utils";
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+import "../styles/index.scss";
+
+const saludo = saludar("hola a todos!");
+
+console.log(saludo);
